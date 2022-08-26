e36c4a1e0c1a65b23f7751fe6491eaa1b6779d4c
Alexey <alexeyzakis@gmail.com>
2022-08-26 03:01:09 +0300

    Changed vars names

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,35 +2,40 @@ import kotlin.random.Random
 
 import myFunctions.roundPlus
 
+const val NUM_OF_ROUNDS = 1_000_000
 const val NUM_OF_DICE_FACES = 6
-const val NUM_OF_ROUNDS = 1_000_000_000
+const val NUM_OF_DICE_VALUE_REPEAT = 1
 
 const val DICE_VALUE_TO_COUNT = 1
 
-var max = 0.0
-var nMax = 0
+var maxProbability = 0.0
+var nToMaxProbability = 0
 
 fun main() {
-    for (numOfDiceFaces in 1..NUM_OF_DICE_FACES * 2)
+    for (numOfDiceFaces in NUM_OF_DICE_VALUE_REPEAT..NUM_OF_DICE_FACES * NUM_OF_DICE_VALUE_REPEAT * 2)
         game(numOfDiceFaces)
-    println("\nN: $nMax, ${roundPlus(max * 100, 10)}%")
+    println("\nBeast num of dices: $nToMaxProbability, ${roundPlus(maxProbability * 100, 10)}%")
 }
 
 fun game(numOfDice: Int) {
-    val stats = Array(numOfDice + 1, {0})
+//    val stats = Array(numOfDice + 1, {0})
+    var numOfDiceValueToCount = 0
     var tempList: MutableList<Int>
     for (i in 0 until NUM_OF_ROUNDS) {
         tempList = mutableListOf()
         for (j in 0 until numOfDice)
             tempList.add(Random.nextInt(1, NUM_OF_DICE_FACES + 1))
-        stats[tempList.count({it == DICE_VALUE_TO_COUNT})] += 1
+        if (tempList.count { it == DICE_VALUE_TO_COUNT } == NUM_OF_DICE_VALUE_REPEAT)
+            numOfDiceValueToCount++
+//        stats[tempList.count({it == DICE_VALUE_TO_COUNT})] += 1
     }
 //    for (k in stats.indices)
 //        println("$k: ${toPercent(stats[k].toFloat(), NUM_OF_ROUNDS)}")
-    val res = stats[1].toDouble() / (NUM_OF_ROUNDS)
-    println("$numOfDice: ${roundPlus(res * 100, 10)}%")
-    if (res > max) {
-        max = res
-        nMax = numOfDice
+//    val res = stats[1].toDouble() / (NUM_OF_ROUNDS)
+    val res = numOfDiceValueToCount.toDouble() / (NUM_OF_ROUNDS)
+    println("Num of dice: $numOfDice: ${roundPlus(res * 100, 10)}%")
+    if (res > maxProbability) {
+        maxProbability = res
+        nToMaxProbability = numOfDice
     }
 }
