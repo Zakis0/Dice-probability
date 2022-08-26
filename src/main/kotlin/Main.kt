0a09869432d2dea03023c20e093eb52d3b8a8b35
Alexey <alexeyzakis@gmail.com>
2022-08-26 14:03:04 +0300

    Changed vars name

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,11 +14,10 @@ var numOfDiceToMaxProbability = 0
 fun main() {
     for (numOfDiceFaces in NUM_OF_DICE_VALUE_REPEAT..NUM_OF_DICE_FACES * NUM_OF_DICE_VALUE_REPEAT * 2)
         game(numOfDiceFaces)
-    println("\nBeast num of dices: $numOfDiceToMaxProbability, ${roundPlus(maxProbability * 100, 10)}%")
+    println("\nBest num of dices: $numOfDiceToMaxProbability, ${roundPlus(maxProbability * 100, 10)}%")
 }
 
 fun game(numOfDice: Int) {
-//    val stats = Array(numOfDice + 1, {0})
     var numOfDiceValueToCount = 0
     var tempList: MutableList<Int>
     for (i in 0 until NUM_OF_ROUNDS) {
@@ -27,13 +26,9 @@ fun game(numOfDice: Int) {
             tempList.add(Random.nextInt(1, NUM_OF_DICE_FACES + 1))
         if (tempList.count { it == DICE_VALUE_TO_COUNT } == NUM_OF_DICE_VALUE_REPEAT)
             numOfDiceValueToCount++
-//        stats[tempList.count({it == DICE_VALUE_TO_COUNT})] += 1
     }
-//    for (k in stats.indices)
-//        println("$k: ${toPercent(stats[k].toFloat(), NUM_OF_ROUNDS)}")
-//    val res = stats[1].toDouble() / (NUM_OF_ROUNDS)
     val res = numOfDiceValueToCount.toDouble() / (NUM_OF_ROUNDS)
-    println("Num of dice: $numOfDice: ${roundPlus(res * 100, 10)}%")
+    println("Num of dices: $numOfDice: ${roundPlus(res * 100, 10)}%")
     if (res > maxProbability) {
         maxProbability = res
         numOfDiceToMaxProbability = numOfDice
